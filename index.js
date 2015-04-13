439b80f1f05ecc2a40e2ff746334b0fe3afd0cd7
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-04-13 13:58:59 +0200

    first working version

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-var github = require('github');
+var github = require('octonode');
+var colors = require('colors');
+var q = require('q')
 var cfg = require('./config');
+
+var deferred = q.defer(),
+    client = github.client(cfg.token),
+    pulls = [];
+
+var promises = [deferred.promise];
+
+cfg.repos.forEach(function (repo, i) {
+  var localDefer = q.defer();
+  promises.push(localDefer.promise);
+
+  client.repo(repo).prs(function(err, result) {
+    if (result.length == 0) { localDefer.resolve() }
+    result.forEach(function (pr) {
+      pulls.push(pr);
+      localDefer.resolve();
+    });
+  });
+  
+  if (i == cfg.repos.length - 1) {
+    deferred.resolve();
+    q.all(promises).then(printPrs);
+  }
+})
+
+function printPrs () {
+  pulls.forEach(function (pull) {
+    var message = pull.title + ' - ' + pull.html_url + ' - ' + pull.updated_at;
+    console.log(message.green);
+  });
+};
+
