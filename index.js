e29ec29b75ba069088893db85a88932b856305ca
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-04-13 14:22:34 +0200

    command line options

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+#!/usr/bin/env node
+
 var github = require('octonode');
 var colors = require('colors');
-var q = require('q')
-var cfg = require('./config');
+var q = require('q');
+var program  = require('commander');
 
+/**
+ * setup command line parsing
+ */
+program
+  .version('1.0.0')
+  .option('-v, --verbose', 'Give more information')
+  .option('-t, --titles', 'Show titles')
+  .option('-s --story', 'Add descriptions to output')
+  .option('-m, --minimal', 'Give only urls')
+  .parse(process.argv);
+
+var cfg = require('./config');
 var deferred = q.defer(),
     client = github.client(cfg.token),
-    pulls = [];
+    pulls = {};
 
 var promises = [deferred.promise];
 
@@ -13,10 +27,12 @@ cfg.repos.forEach(function (repo, i) {
   var localDefer = q.defer();
   promises.push(localDefer.promise);
 
+  pulls[repo] = [];
+
   client.repo(repo).prs(function(err, result) {
     if (result.length == 0) { localDefer.resolve() }
     result.forEach(function (pr) {
-      pulls.push(pr);
+      pulls[repo].push(pr);
       localDefer.resolve();
     });
   });
@@ -28,9 +44,28 @@ cfg.repos.forEach(function (repo, i) {
 })
 
 function printPrs () {
-  pulls.forEach(function (pull) {
-    var message = pull.title + ' - ' + pull.html_url + ' - ' + pull.updated_at;
+  Object.keys(pulls).forEach(function (repo) {
+    console.log('\n' + repo.underline.red);
+    pulls[repo].forEach(function (pull) {
+    var message = '';
+    if (program.minimal) {
+      message += pull.html_url;
+    } else {
+      message += pull.updated_at + ' - ' + pull.html_url;
+    }
+
+    if (program.titles) { 
+      message += ' - ' + pull.title;
+    }
+
+    if (program.story) {
+      message += '\n' + pull.body + '\n';
+    }
+
+    // print all da tings
     console.log(message.green);
+    
+    });
   });
 };
 
