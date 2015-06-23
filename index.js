2ed576d6c49a5d5c0821a5ded71c97c3d7acad1c
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-06-23 10:40:37 +0200

    comply to standard formatting

--- index.js
+++ index.js
@@ -1,106 +1,108 @@
 #!/usr/bin/env node
 
-var github = require('octonode');
-var colors = require('colors');
-var q = require('q');
-var program  = require('commander');
-var prompt = require('prompt');
-var pwuid = require('pwuid');
-var fs = require('fs');
+var github = require('octonode')
+var q = require('q')
+var program = require('commander')
+var prompt = require('prompt')
+var pwuid = require('pwuid')
+var fs = require('fs')
+var version = require('./package.json').version
 
-var pulls = {};
-var configFileLocation = pwuid().dir + '/.pull-me-config.json';
+var pulls = {}
+var configFileLocation = pwuid().dir + '/.pull-me-config.json'
 
 /**
  * setup command line parsing
  */
 program
-  .version('1.0.0')
+  .version(version)
   .option('-a, --add-repositories [type]', 'add repositories comma seperated without spaces[fritzvd/pull-me-tender,fritzvd/harmonize]')
   .option('-m, --minimal', 'Give only urls')
   .option('-t, --titles', 'Show titles')
   .option('-s, --story', 'Add descriptions to output')
-  .parse(process.argv);
+  .parse(process.argv)
 
 try {
-  var cfg = require(configFileLocation);
+  var cfg = require(configFileLocation)
   if (!cfg.token || !cfg.repos) {
-    throw new Error('FILL STUFF in dude');
+    throw new Error('FILL STUFF in dude')
   } else {
-    startGithubPulls();
+    startGithubPulls()
   }
 } catch (e) {
-  console.log("We don't have information on you yet");
-  getUsernameEtc();
+  console.log("We don't have information on you yet")
+  getUsernameEtc()
 }
 
 function getUsernameEtc () {
-  prompt.start();
-  prompt.get(["token", "repos"], function (err, result) {
-   cfg = {
+  prompt.start()
+  prompt.get(['token', 'repos'], function (err, result) {
+    if (err) { throw err }
+    cfg = {
       token: result.token.replace(/(^[ '\^\$\*#&]+)|([ '\^\$\*#&]+$)/g, ''),
-      repos: result.repos.split(',')
-    };
-   fs.writeFileSync(configFileLocation, JSON.stringify(cfg), null, 4);
-   startGithubPulls();
-  });
+  repos: result.repos.split(',')
+    }
+    fs.writeFileSync(configFileLocation, JSON.stringify(cfg), null, 4)
+    startGithubPulls()
+  })
 }
 
 function startGithubPulls () {
   if (program.addRepositories) {
-    cfg.repos = cfg.repos.concat(program.addRepositories.split(','));
-    fs.writeFileSync(configFileLocation, JSON.stringify(cfg, null, 4));
+    cfg.repos = cfg.repos.concat(program.addRepositories.split(','))
+    fs.writeFileSync(configFileLocation, JSON.stringify(cfg, null, 4))
   }
 
-  var deferred = q.defer(),
-      client = github.client(cfg.token);
+  var deferred = q.defer()
+  var client = github.client(cfg.token)
 
-  var promises = [deferred.promise];
+  var promises = [deferred.promise]
 
   cfg.repos.forEach(function (repo, i) {
-    var localDefer = q.defer();
-    promises.push(localDefer.promise);
+    var localDefer = q.defer()
+    promises.push(localDefer.promise)
 
-    pulls[repo] = [];
+    pulls[repo] = []
 
-    client.repo(repo).prs(function(err, result) {
-      if (result.length == 0) { localDefer.resolve() }
+    client.repo(repo).prs(function (err, result) {
+      if (err) { throw err }
+      if (result.length === 0) { localDefer.resolve() }
       result.forEach(function (pr) {
-        pulls[repo].push(pr);
-        localDefer.resolve();
-      });
-    });
-    
-    if (i == cfg.repos.length - 1) {
-      deferred.resolve();
-      q.all(promises).then(printPrs);
+        pulls[repo].push(pr)
+        localDefer.resolve()
+      })
+    })
+
+    if (i === cfg.repos.length - 1) {
+      deferred.resolve()
+      q.all(promises).then(printPrs)
     }
   })
 }
 
 function printPrs () {
   Object.keys(pulls).forEach(function (repo) {
-    console.log('\n' + repo.underline.red);
+    console.log('\n' + repo.underline.red)
     pulls[repo].forEach(function (pull) {
-    var message = '';
-    if (program.minimal) {
-      message += pull.html_url;
-    } else {
-      message += pull.updated_at + ' - ' + pull.html_url;
-    }
+      var message = ''
+      if (program.minimal) {
+        message += pull.html_url
+      } else {
+        message += pull.updated_at + ' - ' + pull.html_url
+      }
 
-    if (program.titles) { 
-      message += ' - ' + pull.title;
-    }
+      if (program.titles) {
+        message += ' - ' + pull.title
+      }
 
-    if (program.story) {
-      message += '\n' + pull.body + '\n';
-    }
+      if (program.story) {
+        message += '\n' + pull.body + '\n'
+      }
 
-    // print all da tings
-    console.log(message.green);
-    
-    });
-  });
-};
+      // print all da tings
+      console.log(message.green)
+
+    })
+  })
+}
 
